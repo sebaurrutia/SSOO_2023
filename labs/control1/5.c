573d3f13911c5d3b77babeebc1caebb0da930f60
sebaurrutia <sebastianandres.urrutia@alumnos.ulagos.cl>
2023-09-07 11:06:01 -0300

    .

--- labs/control1/5.c
+++ labs/control1/5.c
@@ -5,6 +5,14 @@ int main(){
     //Desarrollar un programa que solicite 4 notas en punto flotante por pantalla y se obtenga el
     //promedio simple
     float num1,num2,num3,num4;
+    printf("ingrese 4 notas para calcular el promedio");
+    scanf("%f", & num1);
+    scanf("%f", & num2);
+    scanf("%f", & num3);
+    scanf("%f", & num4);
+    
+    float prom=(num1 + num2 + num3 + num4 )/4;
+    printf("el promedio de las cuatro notas es : %f" ,prom);
     
 
     return 0;
