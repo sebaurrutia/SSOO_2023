38352dd18cbf081d03cc207e8e4d8e4e888c6240
sebaurrutia <sebastianandres.urrutia@alumnos.ulagos.cl>
2023-09-07 11:14:23 -0300

    .

--- labs/control1/6.c
+++ labs/control1/6.c
@@ -3,9 +3,12 @@
 
 int main(){
     float num1,num2,num3;
-    printf("ingrese 3 notas para calcular el promedio ponderado:\n");
+    printf("ingrese 3 notas para calcular el promedio ponderado\n");
+    printf("nota 1 40 porciento :");
     scanf("%f", & num1);
+    printf("\nnota 2 30 porciento :");
     scanf("%f", & num2);
+    printf("\nnota 3 30 porciento :");
     scanf("%f", & num3);
     
     float prom=(num1*0.4 + num2*0.3 + num3*0.3)/10;
